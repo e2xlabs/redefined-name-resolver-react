c0a6c3d274114c2b488bc027e5c8ab8f35fc9776
andrey.ulyanov <andrey.ulyanov@softwea.com>
2023-02-27 14:56:10 +0300

    RED-371 wip

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -13,7 +13,6 @@ root.render(
             eth: ETH_GOERLI_NODE
           }
         }}
-        hiddenAddressGap={{leadingCharLimit: 4, trailingCharLimit: 6}}
         onSelect={(val) => console.log(val)}/>
     </div>
   </React.StrictMode>,
