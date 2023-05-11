17873d457ddc05e4e45fb7fc5b2ae410985a32ef
IvanKuzin <ik@e2xlabs.com>
2023-05-11 13:14:03 +0300

    RED-564 wip

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,24 +2,69 @@ import { RedefinedDomainResolver } from "@redefined/name-resolver-react";
 import React, { useState } from "react";
 
 export default function App() {
-    
+
     const [theme, setTheme] = useState<"light" | "dark">("dark");
-    
+
+    const swapTheme = () => {
+        switch (theme) {
+            case "light":
+                setTheme("dark");
+                return;
+            case "dark":
+                setTheme("light");
+                return;
+        }
+    }
+
+    const openUrl = (url: string) => {
+        window.open(url, "blank");
+    }
+
+    const openCurrentlySupport = () => {
+        openUrl("https://redefined.gitbook.io/connect/integerate-redefined/supported-providers")
+    }
+
+    const openDevGuide = () => {
+        openUrl("https://redefined.gitbook.io/connect/integerate-redefined/name-resolver-js-sdk")
+    }
+
     return (
         <div className={`app ${theme}`}>
             <div className={"app-bar"}>
-                logo : Search
-                
-                <div className={"dev-guide"}>Developer`s Guide</div>
+                <div className={"logo-container"}>
+                    <img src={require("./assets/logo.svg")} />
+                    <span className={"theme-text"}> : Search</span>
+                </div>
+                <div className={"dev-text-container cursor-pointer"} onClick={openDevGuide}>
+                  <div className={"dev-text"}>Developer`s Guide</div>
+                  <img src={require("./assets/arrow.svg")} />
+                </div>
             </div>
             <span className={"title"}>redefined’s Web3 Identity Resolver</span>
-            <div className={"subtitle"}>Integrate redefined SDK or Widget to Search with any Social ID, Domain, Name Service, Email or Username</div>
+            <div className={"subtitle-container"}>
+              <div className={"subtitle theme-text"}>
+                Integrate redefined SDK or Widget to Search with any Social ID, Domain, Name Service, Email or Username
+              </div>
+            </div>
             <div className={"domain-resolver"}>
+                <div className={"theme-btn-container"}>
+                  <div className={"cursor-pointer"} onClick={swapTheme}>
+                    {theme === "dark"
+                      ? <img src={require("./assets/dark.svg")} />
+                      : <img src={require("./assets/light.svg")} />
+                    }
+                  </div>
+                </div>
                 <div className={"domain-resolver-container"}>
                     <RedefinedDomainResolver
                         theme={theme}
-                        width={"50%"}
-                        onUpdate={(val) => console.log(val)}/>
+                        width={"90%"}
+                        onUpdate={(val) => console.log(val)}
+                    />
+                </div>
+                <div className={"dev-text-container support cursor-pointer"} onClick={openCurrentlySupport}>
+                    <div className={"dev-text"}>Currently Support</div>
+                    <img src={require("./assets/arrow.svg")} />
                 </div>
             </div>
         </div>
