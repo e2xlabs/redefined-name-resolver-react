dbd7e16a8ca91304fa255f93017462b5601f5d70
andrey.ulyanov <andrey.ulyanov@softwea.com>
2023-09-01 11:46:58 +0300

    RED-650 wip

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,13 +2,13 @@ import { RedefinedDomainResolver } from "@redefined/name-resolver-react";
 import React, { useState } from "react";
 
 export default function App() {
-    
+
     const currentlySupportUrl = "https://redefined.gitbook.io/connect/integerate-redefined/supported-providers";
     const devGuideUrl = "https://redefined.gitbook.io/connect/integerate-redefined/name-resolver-js-sdk";
     const redefinedOrgUrl = "https://redefined.org";
     const termsConditionsUrl = "https://redefined.org/terms-conditions";
     const privacyUrl = "https://redefined.org/privacy";
-    
+
     const [theme, setTheme] = useState<"light" | "dark">("dark");
 
     const swapTheme = () => {
@@ -64,28 +64,55 @@ export default function App() {
                 Integrate redefined SDK or Widget to Search with any Social ID, Domain, Name Service, Email or Username
               </div>
             </div>
-            <div className={"domain-resolver"}>
-                <div className={"theme-btn-container"}>
-                  <div className={"cursor-pointer"} onClick={swapTheme}>
-                    {theme === "dark"
-                      ? <img src={require("./assets/sun.svg")} />
-                      : <img src={require("./assets/moon.svg")} />
-                    }
-                  </div>
-                </div>
-                <div className={"domain-resolver-container"}>
-                    <RedefinedDomainResolver
-                        theme={theme}
-                        width={"90%"}
-                        onUpdate={(val) => console.log(val)}
-                    />
-                </div>
-                <div className={"dev-text-container support"}>
-                    <div className={"dev-text cursor-pointer link"} onClick={openCurrentlySupport}>
-                      <div>Currently Support</div>
-                      <img src={require("./assets/arrow.svg")} />
+            <div className={"d-flex"}>
+              <div className={"domain-resolver"}>
+                  <div className={"theme-btn-container"}>
+                    <span className={"resolver-type theme-text"}>Resolve</span>
+                    <div className={"cursor-pointer"} onClick={swapTheme}>
+                      {theme === "dark"
+                        ? <img src={require("./assets/sun.svg")} />
+                        : <img src={require("./assets/moon.svg")} />
+                      }
                     </div>
-                </div>
+                  </div>
+                  <div className={"domain-resolver-container"}>
+                      <RedefinedDomainResolver
+                          theme={theme}
+                          width={"90%"}
+                          onUpdate={(val) => console.log(val)}
+                      />
+                  </div>
+                  <div className={"dev-text-container support"}>
+                      <div className={"dev-text cursor-pointer link"} onClick={openCurrentlySupport}>
+                        <div>Currently Support</div>
+                        <img src={require("./assets/arrow.svg")} />
+                      </div>
+                  </div>
+              </div>
+              <div className={"domain-resolver"}>
+                  <div className={"theme-btn-container"}>
+                    <span className={"resolver-type theme-text"}>Reverse</span>
+                    <div className={"cursor-pointer"} onClick={swapTheme}>
+                        {theme === "dark"
+                            ? <img src={require("./assets/sun.svg")} />
+                            : <img src={require("./assets/moon.svg")} />
+                        }
+                    </div>
+                  </div>
+                  <div className={"domain-resolver-container"}>
+                      <RedefinedDomainResolver
+                          theme={theme}
+                          width={"90%"}
+                          onUpdate={(val) => console.log(val)}
+                      />
+                  </div>
+                  <div className={"dev-text-container support"}>
+                      <div className={"dev-text cursor-pointer link"} onClick={openCurrentlySupport}>
+                          <div>Currently Support</div>
+                          <img src={require("./assets/arrow.svg")} />
+                      </div>
+                  </div>
+              </div>
             </div>
             <div className={"footer"}>
                 <div style={{ display: "flex" }}>
